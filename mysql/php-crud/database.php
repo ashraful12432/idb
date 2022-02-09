091b479fa432386fad13eb1a0f81b13a8b8cdd59
ashraful12432 <ashraful15-12432@diu.edu.bd>
2022-02-09 13:02:23 +0600

    php

--- mysql/php-crud/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('host','localhost');
-define('user','root');
-define('pass','');
-define('data','pwad50-php-crud');
-$db=new mysqli(host,user,pass,data);
-?>
--- mysql/php-crud/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('database.php');
-  $id= $_GET['stid'];
-   $sql= "DELETE FROM student WHERE id='$id'";
-   $db->query($sql);
-  if ($db->affected_rows>0){
-      header('location:student_list.php');
-  }
-?>
